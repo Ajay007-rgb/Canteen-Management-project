f942cbfd8f34208166f2a0afd47aab25b4325c26
Ajay <realt8826@gmail.com>
2026-09-25 17:13:49 +0530

    fix keys error

--- config/settings.py
+++ config/settings.py
@@ -167,9 +167,11 @@ RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET', default='')   #Razorpay
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = config('SUPABASE_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = config('SUPABASE_SECRET_KEY')
-AWS_STORAGE_BUCKET_NAME = config('SUPABASE_BUCKET_NAME', default='media')
+AWS_STORAGE_BUCKET_NAME = config('SUPABASE_BUCKET_NAME', default='media')  
 AWS_S3_ENDPOINT_URL = config('SUPABASE_S3_ENDPOINT')
 AWS_S3_REGION_NAME = config('SUPABASE_REGION', default='ap-south-1')
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = False
+
+# fix ho gya
